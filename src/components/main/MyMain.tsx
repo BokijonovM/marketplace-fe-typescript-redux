d1e91b8e11d0bb9abc54aded186e2d6829909a4d
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-12 20:26:52 +0500

    updated

--- src/components/main/MyMain.tsx
+++ src/components/main/MyMain.tsx
@@ -27,17 +27,32 @@ function MyMain() {
         <MyCarousel />
       </div>
       <Container fluid>
-        <div className="my-3">
+        {/* <div className="my-3">
           <h4 className="mb-0">New Products</h4>
+        </div> */}
+        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+            Latest Products
+          </h2>
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {product
+              .slice(0, 8)
+              .reverse()
+              .map((pro) => {
+                return <SingleProduct pro={pro} key={pro._id} />;
+              })}
+          </div>
         </div>
-        <Row xs={1} md={2} lg={4} className="g-4 my-3 row__posters">
-          {product.map((pro) => {
-            return <SingleProduct pro={pro} key={pro._id} />;
-          })}
-        </Row>
       </Container>
     </div>
   );
 }
 
 export default MyMain;
+{
+  /* <Row xs={1} md={2} lg={4} className="g-4 my-3 row__posters">
+  {product.map((pro) => {
+    return <SingleProduct pro={pro} key={pro._id} />;
+  })}
+</Row>; */
+}
--- src/components/main/SingleProduct.tsx
+++ src/components/main/SingleProduct.tsx
@@ -10,22 +10,27 @@ interface ProductDetails {
 function SingleProduct({ pro }: ProductDetails) {
   return (
     <div>
-      <Col>
-        <Card className="my-2">
-          <Card.Img
-            className="card-img-single p-2"
-            variant="top"
+      <div key={pro._id} className="group relative">
+        <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+          <img
             src={pro.imageUrl}
+            alt={pro.name}
+            className="w-full h-full object-center object-cover lg:w-full lg:h-full"
           />
-          <Card.Body>
-            <Card.Title>{pro.name}</Card.Title>
-            <Card.Text>{pro.description}</Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">{pro.brand}</small>
-          </Card.Footer>
-        </Card>
-      </Col>
+        </div>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm text-gray-700">
+              <h5 className="mb-n2">
+                <span aria-hidden="true" className="absolute inset-0" />
+                {pro.name}
+              </h5>
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">{pro.brand}</p>
+          </div>
+          <p className="text-sm font-medium text-gray-900">${pro.price}</p>
+        </div>
+      </div>
     </div>
   );
 }
