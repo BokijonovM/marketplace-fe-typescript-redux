beddd966c6f28ec5903e5f323ed7cde2d262f202
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-03-12 16:16:15 +0500

    updated

--- src/components/main/MyMain.tsx
+++ src/components/main/MyMain.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col, Container, Card } from "react-bootstrap";
+import { Product } from "../../types/index";
 
 function MyMain() {
+  const [product, setProduct] = useState<Product[]>([]);
+  const fetchProduct = async () => {
+    try {
+      let res = await fetch(process.env.REACT_APP_API + "product");
+      if (res.ok) {
+        let data = await res.json();
+        console.log(data);
+        setProduct(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchProduct();
+  }, []);
   return (
     <div>
       <Container fluid>
